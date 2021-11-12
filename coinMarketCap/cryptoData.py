ee30a7b9061939e542631c991c886794705d7de4
Robert Goodwin <rgoodw3@uic.edu>
2021-11-11 19:58:25 -0600

    start of the crypto data scraper

---
+++ coinMarketCap/cryptoData.py
@@ -0,0 +1,24 @@
+import validation
+from requests import Request, Session
+import json
+import pprint
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+parameters = {
+    'slug':'bitcoin',
+    'convert':'USD'
+}
+
+headers = {
+    'Accepts':'application/json',
+    'X-CMC_PRO_API_KEY':validation.api_key
+}
+
+
+session = Session()
+session.headers.update(headers)
+
+response = session.get(url,params=parameters)
+data = json.loads(response.text)
+pprint.pprint(data['data']['1']['quote']['USD']['price'])
---
+++ coinMarketCap/validation.py
@@ -0,0 +1,1 @@
+api_key = '' # INSERT YOUR API KEY HERE
