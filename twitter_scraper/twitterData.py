99effbed04ae168c039488c69e9f557a1ae668c4
Kris Rusin <kris.rusin@gmail.com>
2021-11-12 23:37:50 -0600

    retrieved some tweets and put them into files. might need alternate way to retrieve tweets since twitter api only allows up 3200. We have 1000 and only goes back a few months

--- twitter_scraper/twitterData.py
+++ twitter_scraper/twitterData.py
@@ -2,42 +2,29 @@ import validation
 import tweepy
 import json
 import pprint
+import csv
+import datetime
 
 
-
-def get_tweets(username):
+def get_tweets():
         # Authorization to consumer key and consumer secret
         auth = tweepy.OAuthHandler(validation.consumer_key, validation.consumer_secret)
   
         # Access to user's access key and access secret
         auth.set_access_token(validation.access_key, validation.access_secret)
-  
         # Calling api
         api = tweepy.API(auth)
-  
-        # 200 tweets to be extracted
-        number_of_tweets=20000
-        tweets = api.user_timeline(screen_name='elonmusk',count = number_of_tweets)
-        
-        
-        
-        # This code print out the first tweet pulled in JSON format
-        # data = tweets[0]._json
-        # pprint.pprint(data)
-        # Empty Array
+        user = api.get_user(screen_name='@elonmusk')
+        tweets = []
+        csvFile = open('elonsTweets2.csv', 'w')
+        csvWriter = csv.writer(csvFile)
+        pages = tweepy.Cursor(api.user_timeline,user_id=user.id_str, count=200,include_rts=True).pages(16)
 
-        tmp=[] 
-  
-        # create array of tweet information: username, 
-        # tweet id, date/time, text
-        tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created 
-        for j in tweets_for_csv:
-  
-            # Appending tweets to the empty array tmp
-            tmp.append(j) 
-  
-        # Printing the tweets
-        for i in tmp:
-            print(i+"\n")
+        for page in pages:
+            for tweet in page:
+                csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+                tweets.append(tweet)
+                print(tweet.text)
+        csvFile.close()
 
-get_tweets("twitter-handle") 
+get_tweets() 
